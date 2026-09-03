5a04c8261eef89d6619f23a6476e7315ac23909c
manhtinh205703-art <manhtinh205703@gmail.com>
2026-09-03 10:14:27 +0700

    Add files via upload

---
+++ mavenproject2/Mavenproject2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.tinh123.mavenproject2;
+
+/**
+ *
+ * @author HN PC STORE
+ */
+public class Mavenproject2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
